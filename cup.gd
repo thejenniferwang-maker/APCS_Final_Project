321d128b577f2d0a4e61f849cf80b7e13e057084
thejenniferwang-maker <thejenniferwang@gmail.com>
2026-05-07 15:09:31 -0700

    added a print statement

--- cup.gd
+++ cup.gd
@@ -7,5 +7,6 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			$ "The Drink".visible = true
+			print("hellO")
 	# $ tell Godot to look inside the current node for a child node
 	
