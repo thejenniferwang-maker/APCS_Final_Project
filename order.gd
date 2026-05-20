6eb44ce9e47e5c93f458977159ebc534a926f4bb
titan3-code <tiffanytan2026@gmail.com>
2026-05-20 00:52:40 -0700

    Instantiate star scene to ui scene (5/18/26 )

--- order.gd
+++ order.gd
@@ -19,7 +19,6 @@ func _ready() -> void:
 	if drinkOrder == "taro" and toppingOrder == "jelly":
 		$taroAndJelly.visible = true
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- stars.gd
+++ stars.gd
@@ -3,8 +3,24 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	# One star rewarded by default
+	$"star1".visible = true
+	#$"star2".visible = false
+	#$"star3".visible = false
+	
+	var drinkMade = "regular"	# UPDATE VAR LATER
+	var drinkMatched = true if $drinkOrder == drinkMade else false
+	if drinkMatched == true:
+		$"star2".visible = true
+	
+	var toppingAdded = "tapioca"	# UPDATE VAR LATER
+	var toppingMatched = true if $toppingOrder == toppingAdded else false
+	if drinkMatched == true:
+		$"star3".visible = true
+		
+	# THOUGHTS:
+	# Use array to keep track of stuff made and/or ordered?# and compare it; if true then make star visible (same time)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
