37d1da57a87772a1c1c6f0a1b419fa522a7203d3
thejenniferwang-maker <thejenniferwang@gmail.com>
2026-05-20 14:07:34 -0700

    changed the buttons and it works now

--- boba.gd
+++ boba.gd
@@ -1,1 +1,8 @@
 extends TextureRect
+
+var click_count = 0
+func _on_boba_topping_button_down() -> void:
+	if click_count == 0:
+		$"Boba".visible = true
+		
+	
--- regular_cup.gd
+++ regular_cup.gd
@@ -1,23 +1,7 @@
 extends Panel
-	#Regular
-	
+
 var click_count = 0
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			
-			if click_count == 0:
-				$ "The Drink".visible = true
-				click_count += 1 # Set to 1 so the next click 
-			elif click_count == 1:
-				$ "Boba".visible = true
-				click_count += 1
- 
-
-
 func _on_regular_button_down() -> void:
-	pass # Replace with function body.
-
-
-func _on_taro_flavor_button_down() -> void:
-	hide()
+	if click_count == 0:
+		$"The Drink".visible = true
+		
